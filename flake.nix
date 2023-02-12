5f6494e060703c5564c0ef929d0a02b23f98d2b1
Shebang <celestecaprine@protonmail.com>
2023-02-12 00:21:21 -0600

    Added hostname argument in np-t430 section

--- flake.nix
+++ flake.nix
@@ -67,7 +67,12 @@
     homeConfigurations = {
       "shebang@np-t430" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs;}; # Pass flake inputs to our config
+        extraSpecialArgs = {
+          inherit inputs;
+          host = {
+            hostName = "np-t430";
+          };
+        }; # Pass flake inputs to our config
         # > Our main home-manager configuration file <
         modules = [./home-manager/home.nix];
       };
